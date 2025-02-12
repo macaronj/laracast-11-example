5de5d820dec0242fbf6d100c12d25f8efd051e4d
Jonas Unkel <macaronj@protonmail.com>
2025-02-12 12:27:23 +0100

    first commit

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,4 @@
+<x-layout>
+    <x-slot:heading>Contact</x-slot:heading>
+    <h1>Hallo von Contact-Seite!</h1>
+</x-layout>
---
+++ resources/views/home.blade.php
@@ -0,0 +1,4 @@
+<x-layout>
+    <x-slot:heading>Home</x-slot:heading>
+    <h1>Hallo von der Home-Seite!</h1>
+</x-layout>
