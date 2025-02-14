fe0cd59b6810bf69d1738b8cb59e71d4e17b6621
Jonas Unkel <macaronj@protonmail.com>
2025-02-14 17:11:37 +0100

    Day 11

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -4,15 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Job extends Model
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
 
     use HasFactory;
-    use Notifiable;
+
     protected $table = 'job_listings';
 
     protected $fillable = ['title', 'salary'];
+
+    public function employer()
+    {
+        return $this->belongsTo(Employer::class);
+    }
 }
